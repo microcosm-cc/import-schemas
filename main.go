14d1ada4acd291d9721fafe01be34592e7c8ecc5
Matt Cottingham <matt.cottingham@gmail.com>
2014-05-19 14:41:20 +0100

    Walk users directory and build map of ID -> filepath

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/microcosm-cc/goconfig"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const (
+	configFile          = "config.toml"
+	configExportSection = "export"
+	usersPath           = "users"
+)
+
+func storeID(itemType string, path string, itemMap map[int]string) {
+	// Split path on itemType (e.g. "users")
+	strID := strings.Split(path, itemType)[1]
+	// Split file extension
+	strID = strings.Split(strID, ".json")[0]
+	// Replace path delimeters to extract ID.
+	strID = strings.Replace(strID, "/", "", -1)
+
+	// Store ID and associated file path in itemMap.
+	ID, err := strconv.Atoi(strID)
+	if err != nil {
+		log.Print(err)
+	}
+	itemMap[ID] = path
+}
+
+func main() {
+
+	// Load config.
+	config, err := goconfig.ReadConfigFile(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rootpath, err := config.GetString(configExportSection, "rootpath")
+	if err != nil {
+		log.Print(err)
+	}
+
+	// Build a map of User ID -> file path.
+	userMap := make(map[int]string)
+
+	// Define what should be done in the subsequent directory walk.
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		if filepath.Ext(path) == ".json" {
+			storeID(usersPath, path, userMap)
+		}
+		return nil
+	}
+
+	// Walk the each path in the "users" directory.
+	filepath.Walk(path.Join(rootpath, usersPath), walkFunc)
+
+	var keys []int
+	for key, _ := range userMap {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	log.Print(keys)
+
+}
