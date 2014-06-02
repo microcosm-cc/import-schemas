fac63abda0c3e35211a3324cc59d9aa8ca7b3f71
Matt Cottingham <matt.cottingham@gmail.com>
2014-06-02 17:41:02 +0100

    Conversation WIP

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/lib/pq"
+	exports "github.com/microcosm-cc/export-schemas/go/forum"
 	"github.com/microcosm-cc/import-schemas/config"
+	"github.com/microcosm-cc/import-schemas/walk"
+	"io/ioutil"
 	"log"
+	"sort"
 )
 
 func main() {
@@ -23,11 +28,11 @@ func main() {
 	}
 
 	// Load all users, then create a single user entry corresponding to the site owner.
-	owner, users, err := LoadUsers(config.Rootpath, config.SiteOwnerId)
+	exOwner, users, err := LoadUsers(config.Rootpath, config.SiteOwnerId)
 	if err != nil {
 		log.Fatal(err)
 	}
-	ownerId, err := StoreUser(db, owner)
+	ownerId, err := StoreUser(db, exOwner)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,7 +44,7 @@ func main() {
 		Description:  config.SiteDesc,
 		ThemeId:      1,
 	}
-	siteId, profileId, err := CreateOwnedSite(db, owner.Name, ownerId, site)
+	siteId, profileId, err := CreateOwnedSite(db, exOwner.Name, ownerId, site)
 	log.Printf("New site ID: %d\n", siteId)
 	log.Printf("Owner profile ID: %d\n", profileId)
 
@@ -51,18 +56,20 @@ func main() {
 	log.Printf("Origin ID: %d\n", originId)
 
 	// Now record the import of the site owner
-	err = RecordImport(db, originId, ItemTypeUser, owner.Id, ownerId)
+	err = RecordImport(db, originId, ItemTypeUser, exOwner.ID, ownerId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Finally, store the rest of the users and create a profile for each.
+	// Store the remaining users and create a profile for each.
+	importedProfiles := make(map[int64]int64)
+
 	for _, user := range users {
 		newUserId, err := StoreUser(db, user)
 		if err != nil {
 			log.Print(err)
 		}
-		err = RecordImport(db, originId, ItemTypeUser, user.Id, newUserId)
+		err = RecordImport(db, originId, ItemTypeUser, user.ID, newUserId)
 		if err != nil {
 			log.Print(err)
 		}
@@ -76,10 +83,82 @@ func main() {
 			ProfileName:       user.Name,
 			AvatarUrlNullable: avatarUrl,
 		}
-		err = StoreProfile(db, profile)
+		profileID, err := StoreProfile(db, profile)
 		if err != nil {
 			log.Print(err)
 		}
+
+		importedProfiles[user.ID] = profileID
+		fmt.Printf(".")
 	}
+
+	// Forums
+	fMap, err := walk.WalkExports(config.Rootpath, "forums")
+	var fKeys []int
+	for key, _ := range fMap {
+		fKeys = append(fKeys, key)
+	}
+	sort.Ints(fKeys)
+
+	for _, ID := range fKeys {
+		bytes, err := ioutil.ReadFile(fMap[ID])
+		if err != nil {
+			log.Printf("Error opening path: %d\n", ID)
+			continue
+		}
+		exForum := exports.Forum{}
+		err = json.Unmarshal(bytes, &exForum)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		// CreatedBy/OwnedBy are assumed to be the site owner.
+		m := Microcosm{
+			Title:       exForum.Name,
+			Description: exForum.Text,
+			CreatedBy:   ownerId,
+			OwnedBy:     ownerId,
+			IsOpen:      exForum.Open,
+			IsSticky:    exForum.Sticky,
+			IsModerated: exForum.Moderated,
+			IsDeleted:   exForum.Deleted,
+		}
+		mID, err := StoreMicrocosm(db, m)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		log.Print(mID)
+		err = RecordImport(db, originId, ItemTypeMicrocosm, exForum.ID, mID)
+		log.Print(err)
+	}
+
+	// Conversations
+	/*
+		    cMap, err := walk.WalkExports(config.Rootpath, "conversations")
+
+			var keys []int
+			for key, _ := range cMap {
+				keys = append(keys, key)
+			}
+			sort.Ints(keys)
+
+			// Iterate map in order
+			for _, ID := range keys {
+				bytes, err := ioutil.ReadFile(exportsMap[ID])
+				if err != nil {
+					log.Printf("Error opening path: %d\n", ID)
+					continue
+				}
+				exConv := exports.Conversation{}
+				err = json.Unmarshal(bytes, &exConv)
+				if err != nil {
+					log.Print(err)
+					continue
+				}
+
+			}
+	*/
 
 }
