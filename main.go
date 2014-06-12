10795983b11876c32d0ad36605fadc4b229839b5
David Kitchen <david@buro9.com>
2014-06-12 22:05:58 +0100

    Need both CPU and memory profiling to enable me to figure out why profile importing was so slow... answer? HTTP gets fetching avatars from gravatar for new users. So we're now skipping that if we import and we'll load custom avatars in a second concurrent pass.

--- main.go
+++ main.go
@@ -2,19 +2,95 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
 
-	// Included for the side effect of configuring the logger via flag.Parse()
-	_ "github.com/golang/glog"
+	"github.com/golang/glog"
 
-	// "github.com/microcosm-cc/microcosm/cache"
+	"github.com/microcosm-cc/microcosm/cache"
 	h "github.com/microcosm-cc/microcosm/helpers"
 
 	"github.com/microcosm-cc/import-schemas/config"
 	"github.com/microcosm-cc/import-schemas/imp"
 )
 
+var memprof = flag.String("memprof", "", "write memory profile to file")
+var cpuprof = flag.String("cpuprof", "", "write cpu profile to file")
+
 func main() {
 	flag.Parse()
+
+	// Go as fast as we can
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	if *memprof != "" {
+		// Reference time is used for formatting.
+		// See http://golang.org/pkg/time for details.
+		f, err := os.Create(*memprof)
+		if err != nil {
+			glog.Fatal(err)
+		}
+
+		// Catch SIGINT and write heap profile
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		go func() {
+			for sig := range c {
+				glog.Warningf("Caught %v, stopping profiler and exiting..", sig)
+				// Heap profiler is run on GC, so make sure it GCs before exiting.
+				runtime.GC()
+				pprof.WriteHeapProfile(f)
+				f.Close()
+				glog.Flush()
+				os.Exit(1)
+			}
+		}()
+	} else {
+		// Catch closing signal and flush logs
+		sigc := make(chan os.Signal, 1)
+		signal.Notify(
+			sigc,
+			syscall.SIGHUP,
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGQUIT,
+		)
+		go func() {
+			<-sigc
+			glog.Flush()
+			os.Exit(1)
+		}()
+	}
+
+	if *cpuprof != "" {
+		// Reference time is used for formatting.
+		// See http://golang.org/pkg/time for details.
+		f, err := os.Create(*cpuprof)
+		if err != nil {
+			glog.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+
+		// Catch closing signal and flush logs
+		sigc := make(chan os.Signal, 1)
+		signal.Notify(
+			sigc,
+			syscall.SIGHUP,
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGQUIT,
+		)
+		go func() {
+			<-sigc
+			pprof.StopCPUProfile()
+			glog.Flush()
+			os.Exit(1)
+		}()
+	}
 
 	h.InitDBConnection(h.DBConfig{
 		Host:     config.DbHost,
@@ -24,9 +100,9 @@ func main() {
 		Password: config.DbPass,
 	})
 
-	// // If you want to use memcache to help speed things up... the cache misses
-	// // may not make this as fast as you hope though
-	// cache.InitCache("localhost", 11211)
+	// If you want to use memcache to help speed things up... the cache misses
+	// may not make this as fast as you hope though
+	cache.InitCache("localhost", 11211)
 
 	imp.Import()
 }
