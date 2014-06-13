2e33eccbcdeabb5020df9a27954822728beae426
Matt Cottingham <matt.cottingham@gmail.com>
2014-06-13 16:53:21 +0100

    Initial work for using models.CommentSummaryType to import comments

--- imp/comment.go
+++ imp/comment.go
@@ -4,39 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/cheggaaa/pb"
 	"github.com/golang/glog"
 
 	src "github.com/microcosm-cc/export-schemas/go/forum"
 	h "github.com/microcosm-cc/microcosm/helpers"
+	"github.com/microcosm-cc/microcosm/models"
 
 	"github.com/microcosm-cc/import-schemas/accounting"
 	"github.com/microcosm-cc/import-schemas/conc"
 	"github.com/microcosm-cc/import-schemas/files"
 )
-
-// Comment struct
-type Comment struct {
-	ItemTypeID      int64
-	ItemID          int64
-	ProfileID       int64
-	Created         time.Time
-	InReplyTo       sql.NullInt64
-	IsVisible       bool
-	IsModerated     bool
-	IsDeleted       bool
-	AttachmentCount int64
-}
-
-// CommentRevision struct
-type CommentRevision struct {
-	CommentID int64
-	ProfileID int64
-	Raw       string
-	Created   time.Time
-}
 
 // Walk directory and load all comments into a map ID -> Filepath
 // Iterate the map, loading the files into src.Comments
@@ -51,7 +30,6 @@ func importComments(args conc.Args) (errors []error) {
 
 	// Comments.
 	args.ItemTypeID = h.ItemTypes[h.ItemTypeComment]
-
 	glog.Info("Loading comments...")
 
 	err := files.WalkExportTree(args.RootPath, args.ItemTypeID)
@@ -59,7 +37,9 @@ func importComments(args conc.Args) (errors []error) {
 		exitWithError(err, errors)
 	}
 
+	// Map of old comment ID to pointer of loaded comment and IDs of replies.
 	loadedNodeMap := make(map[int64]*Node)
+	// Roots are comments where InReplyTo is 0.
 	roots := []int64{}
 
 	commentIDs := files.GetIDs(args.ItemTypeID)
@@ -99,15 +79,14 @@ func importComments(args conc.Args) (errors []error) {
 	}
 	bar.Finish()
 
-	fmt.Printf("Found %d roots\n", len(roots))
-	glog.Info("Found %d roots\n", len(roots))
+	glog.Info("Found %d comment roots\n", len(roots))
 
 	// Iterate the roots, storing each root first, then doing breadth-first traversal and storing all replies.
 	taskBar := pb.StartNew(len(roots))
 	var wg sync.WaitGroup
 	tasks := make(chan int64, len(roots)+1)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 50; i++ {
 		wg.Add(1)
 		go func() {
 			for ID := range tasks {
@@ -119,6 +98,7 @@ func importComments(args conc.Args) (errors []error) {
 				}
 				taskBar.Increment()
 			}
+			wg.Done()
 		}()
 	}
 
@@ -198,7 +178,7 @@ func importComment(args conc.Args, itemID int64, srcComment src.Comment) error {
 		)
 	}
 
-	// The comment this comment replies to may have been imported previously.
+	// The comment this comment replies to should have been imported previously.
 	if srcComment.InReplyTo > 0 {
 		replyToID := accounting.GetNewID(
 			args.OriginID,
@@ -208,13 +188,10 @@ func importComment(args conc.Args, itemID int64, srcComment src.Comment) error {
 		if replyToID > 0 {
 			srcComment.InReplyTo = replyToID
 		} else {
-			// Log that InReplyTo wasn't found.
-			if glog.V(2) {
-				glog.Infof(
-					"InReplyTo for comment ID %d does not have an imported ID",
-					itemID,
-				)
-			}
+			glog.Errorf(
+				"InReplyTo for comment ID %d does not have an imported ID",
+				itemID,
+			)
 		}
 	}
 
@@ -227,16 +204,22 @@ func importComment(args conc.Args, itemID int64, srcComment src.Comment) error {
 		inReplyTo = sql.NullInt64{Valid: true, Int64: srcComment.InReplyTo}
 	}
 
-	comment := Comment{
-		ItemTypeID:      h.ItemTypes[h.ItemTypeConversation],
-		ItemID:          conversationID,
-		ProfileID:       createdByID,
-		Created:         srcComment.DateCreated,
-		InReplyTo:       inReplyTo,
-		IsVisible:       visible,
-		IsModerated:     srcComment.Moderated,
-		IsDeleted:       srcComment.Deleted,
-		AttachmentCount: 0,
+	comment := models.CommentSummaryType{
+		ItemType:          "conversation",
+		ItemId:            conversationID,
+		InReplyToNullable: inReplyTo,
+		Markdown:          srcComment.Versions[0].Text,
+	}
+	comment.Meta.CreatedById = createdByID
+	comment.Meta.Created = srcComment.DateCreated
+	comment.Meta.Flags.Visible = visible
+	comment.Meta.Flags.Moderated = srcComment.Moderated
+	comment.Meta.Flags.Deleted = srcComment.Deleted
+
+	_, err := comment.Import(args.SiteID)
+	if err != nil || comment.Id < 1 {
+		glog.Errorf("Failed to import comment for conversation %d: %s", srcComment.ID, err)
+		return err
 	}
 
 	tx, err := h.GetTransaction()
@@ -246,42 +229,12 @@ func importComment(args conc.Args, itemID int64, srcComment src.Comment) error {
 	}
 	defer tx.Rollback()
 
-	iCID, err := createComment(tx, comment)
-	if err != nil {
-		glog.Errorf("Failed to createComment for conversation %d: %+v", itemID, err)
-		return err
-	}
-
-	// Fetch new ID of revision creator.
-	revisionAuthorID := accounting.GetNewID(
-		args.OriginID,
-		h.ItemTypes[h.ItemTypeProfile],
-		srcComment.Versions[0].Editor,
-	)
-	// Fall back to original comment author.
-	if revisionAuthorID == 0 {
-		revisionAuthorID = createdByID
-	}
-
-	revision := CommentRevision{
-		CommentID: iCID,
-		ProfileID: revisionAuthorID,
-		Raw:       srcComment.Versions[0].Text,
-		Created:   srcComment.Versions[0].DateModified,
-	}
-
-	_, err = createRevision(tx, revision)
-	if err != nil {
-		glog.Errorf("Failed to createRevision for CommentID: %d", srcComment.ID)
-		return err
-	}
-
 	err = accounting.RecordImport(
 		tx,
 		args.OriginID,
 		args.ItemTypeID,
 		srcComment.ID,
-		iCID,
+		comment.Id,
 	)
 	if err != nil {
 		glog.Errorf("Failed to recordImport: %+v", err)
@@ -294,56 +247,5 @@ func importComment(args conc.Args, itemID int64, srcComment src.Comment) error {
 		return err
 	}
 
-	if glog.V(2) {
-		glog.Infof("Successfully imported conversation %d", itemID)
-	}
 	return nil
-}
-
-// createComment puts a single comment into the database.
-func createComment(tx *sql.Tx, c Comment) (cID int64, err error) {
-	err = tx.QueryRow(
-		`INSERT INTO comments (
-            item_type_id, item_id, profile_id, created, is_visible,
-            is_moderated, is_deleted, in_reply_to, attachment_count, yay_count,
-            meh_count, grr_count
-        ) VALUES (
-            $1, $2, $3, $4, $5,
-            $6, $7, $8, 0, 0,
-            0, 0
-        ) RETURNING comment_id;`,
-		c.ItemTypeID,
-		c.ItemID,
-		c.ProfileID,
-		c.Created,
-		c.IsVisible,
-		c.IsModerated,
-		c.IsDeleted,
-		c.InReplyTo,
-	).Scan(&cID)
-	return
-}
-
-// createRevision puts a single comment into the database.
-func createRevision(tx *sql.Tx, r CommentRevision) (rID int64, err error) {
-	err = tx.QueryRow(
-		`INSERT INTO revisions (
-            comment_id,
-            profile_id,
-            "raw",
-            created,
-            is_current
-        ) VALUES (
-            $1,
-            $2,
-            $3,
-            $4,
-            true
-        ) RETURNING revision_id`,
-		r.CommentID,
-		r.ProfileID,
-		r.Raw,
-		r.Created,
-	).Scan(&rID)
-	return
 }
