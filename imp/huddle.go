e9845999e6a246bd4d9f8e57edc9e63386e6e8fe
David Kitchen <david@buro9.com>
2014-07-25 10:49:15 +0100

    Removed huddle.MarkAsRead

--- imp/huddle.go
+++ imp/huddle.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"strings"
-	"time"
 
 	"github.com/golang/glog"
 
@@ -147,17 +146,6 @@ func importHuddle(args conc.Args, itemID int64) error {
 		if !strings.Contains(err.Error(), "links_url_key") {
 			glog.Errorf("Failed to import comment for huddle %d: %s", srcMessage.ID, err)
 			return err
-		}
-	}
-
-	// Mark the sent version of the huddle as read
-	_, err = models.MarkAsRead(h.ItemTypes[h.ItemTypeHuddle], huddle.Id, authorID, time.Now())
-
-	// Mark the huddle as read.
-	for _, p := range huddle.Participants {
-		_, err := models.MarkAsRead(h.ItemTypes[h.ItemTypeHuddle], huddle.Id, p.Id, time.Now())
-		if err != nil {
-			glog.Infof("Could not MarkAsRead huddle %d for recipient %d", huddle.Id, p.Id)
 		}
 	}
 
